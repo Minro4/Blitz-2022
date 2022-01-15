ac251e606379420e259c8d2bbfe562997cf6bf9d
osuw3401 <william.osullivan-dargis@usherbrooke.ca>
2022-01-14 22:23:21 -0500

    unit manager

--- UnitManager.cs
+++ UnitManager.cs
@@ -1,21 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Blitz2022
 {
     public class UnitManager
     {
+        public List<Unit> units;
         public static void ConvertTeamUnits(GameMessage message)
         {
             foreach (var team in message.teams)
             {
-                Console.WriteLine(team.units);
                 team.units = team.units.Select(Unit.Factory).ToList();
             }
         }
         public UnitManager(GameMessage message)
         {
-             
+            units = message.teams.SelectMany(team => team.units).ToList();
         }
     }
 }
